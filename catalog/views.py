1d1bd7dcc2eb710670fc698aae747740aedaf7e1
Dmitry Shvarev <dima.dreadman@mail.ru>
2026-09-10 00:27:24 +0500

    add views POST

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
@@ -6,4 +7,11 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        # Отображаем сообщение об успешной отправке данных
+        return HttpResponse(f"Спасибо, {name}! Ваши данные успешно отправлены.")
     return render(request, 'contacts.html')
