fb9adb7051ef35678deeaa887b53b82cde31c383
Dmitry Shvarev <dima.dreadman@mail.ru>
2026-09-16 02:33:24 +0500

    add models Product and Category

--- config/settings.py
+++ config/settings.py
@@ -86,8 +86,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 MAILERS = {
     'default': {
